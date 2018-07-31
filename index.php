d2cb767226968676785d977961b8d9d733c5745a
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 11:17:42 +0200

    correction

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$number = 1;
+$number = 1; // définition de la variable de départ
   ?>
   <!DOCTYPE html>
   <html lang="fr" dir="ltr">
@@ -9,13 +9,17 @@ $number = 1;
     </head>
     <body>
       <p>
-<?php
-while ($number <= 10 )
-{
-  echo $number . ' ';
-  $number = $number + ($number / 2);
-}
- ?>
+        <?php
+        //défintion de la boucle
+          while ($number <= 10 ){ //tant que $number est inférieur ou égale à 10
+          echo $number . ' '; // afichage des résultats
+          $number += $number / 2; // ajout de la moitié de sa valeur
+          }
+//variante avec une boucle for
+          for($number = 1; $number <= 10; $number += $number / 2){ // pour $number est égale à 1, inférieur ou égale à 10, et ajout de la moité de sa valeur
+          echo $number . ' '; // affichage des résultats
+          }
+         ?>
       </p>
     </body>
   </html>
